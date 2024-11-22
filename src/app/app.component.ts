06ed9c629b001c23fc0980296790c3c746e2a29e
Angel Garcia <angsevilla@gmail.com>
2024-11-22 11:26:43 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+/* Dentro del decorador podemos crear el HTML mediante la variable template como se puede ver a continuacion:
+template: `<h1>{{title}}</h1>
+<hr>
+<h3>Counter: {{ counter }}</h3>`
+Tambien se podría meter el style metiendo el css en el mediante la variable style
+*/
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bases';
+  //title = 'Mi primera app de Angular' - El profe prefiere la siguiente sintaxis para mejorar la legibilidad
+  public title:string = 'App';
+
 }
